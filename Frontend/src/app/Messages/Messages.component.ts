92584bab7266c510eff33305d5eea3c64cfcf56e
Shabo-95 <Shabo-95@hotmail.com>
2024-05-31 16:01:29 +0200

    Login Component is now created

---
+++ Frontend/src/app/Messages/Messages.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Messages',
+  templateUrl: './Messages.component.html',
+  styleUrls: ['./Messages.component.scss'],
+})
+export class MessagesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountComponent } from './Account/Account.component';
 import { LoginComponent } from './Login/Login.component';
+import { MessagesComponent } from './Messages/Messages.component';
 
 const routes: Routes = [
   {
-    path: 'login',
+    path: '',
     component: LoginComponent,
   },
   {
     path: 'account',
     component: AccountComponent,
+  },
+  {
+    path: 'messages',
+    component: MessagesComponent,
   },
 ];
 
